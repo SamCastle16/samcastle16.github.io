94466597b12ca03417481fb3635b5c88a75c3e15
SamCastle16 <sandyamc10@gmail.com>
2021-08-03 09:30:34 -0400

    Agregue scroll reveal

---
+++ main.js
@@ -0,0 +1,10 @@
+// document.querySelector(".menu-btn").addEventListener("click", () => {
+//   document.querySelector(".nav-menu").classList.toggle("show");
+// });
+
+// ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.proyectos', { delay: 500 });
+ScrollReveal().reveal('.mr', { delay: 300 });
+ScrollReveal().reveal('.teque', { delay: 300 });
+ScrollReveal().reveal('.sond', { delay: 300 });
+ScrollReveal().reveal('.footer', { delay: 500 });
